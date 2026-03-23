02e88ce4f9d62a0f700166ef2d5f66fa36da0773
Beatriz Rosa <142547489+beatrizrosaa@users.noreply.github.com>
2026-03-23 22:29:05 +0000

    Tarega testada

--- tests/appTest.py
+++ tests/appTest.py
@@ -7,6 +7,24 @@ class AppTest(unittest.TestCase):
 
     def test_print_health_check(self):
         response = self.app.get('/health-check')
-        self.assertEqual(200, response.status_code, "Erro no test_http_code!")
-        self.assertEqual("<h1>Hello, I'm Alive!</h1>", response.get_data(as_text=True)
-                          , "Erro no test_print_health_check!")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual("<h1>Hello, I'm Alive!</h1>", response.get_data(as_text=True))
+
+    def test_print_hello_success(self):
+        # Para o sucesso, precisamos passar o parâmetro ?name=
+        response = self.app.get('/hello?name=Beatriz')
+        
+        self.assertEqual(response.status_code, 200)
+        # O retorno esperado é "Hello, Beatriz!" conforme seu app.py
+        self.assertEqual("Hello, Beatriz!", response.get_data(as_text=True))
+
+    def test_print_hello_error(self):
+        # O erro no seu app.py acontece quando o 'name' NÃO é informado
+        # Isso vai executar a linha: return "Nome não informado", 400
+        response = self.app.get('/hello') 
+        
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual("Nome não informado", response.get_data(as_text=True))
+
+if __name__ == '__main__':
+    unittest.main()
